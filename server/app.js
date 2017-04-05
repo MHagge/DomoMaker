f49b44c6cab2d440f163ffc78dac58785fc7d671
MHagge <mhh5546@rit.edu>
2017-04-05 14:53:12 -0400

    Maker B Upload

--- server/app.js
+++ server/app.js
@@ -2,22 +2,23 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
-const cookieParser  = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
 
 const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
-mongoose.connect(dbURL, (err)=>{
-  if(err){
+mongoose.connect(dbURL, (err) => {
+  if (err) {
     console.log('Could not connect to database');
     throw err;
   }
 });
-//pull in our routes
+// pull in our routes
 const router = require('./router.js');
 
 const app = express();
@@ -26,17 +27,23 @@ app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-  extended:true,
+  extended: true,
 }));
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main'}));
+app.use(session({
+  key: 'sessionid',
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
 
 router(app);
 
-app.listen(PORT, (err)=>{
-  if(err){
+app.listen(PORT, (err) => {
+  if (err) {
     throw err;
   }
   console.log(`Listening on port ${PORT}`);
--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -1,36 +1,40 @@
 const models = require('../models');
 const Account = models.Account;
-const loginPage = (req, res) =>{
+const loginPage = (req, res) => {
   res.render('login');
 };
 
-const signupPage = (req, res) =>{
+const signupPage = (req, res) => {
   res.render('signup');
 };
-const logout = (req, res) =>{
+const logout = (req, res) => {
+  req.session.destroy();
   res.redirect('/');
-}
+};
 
-const login = (request, response) =>{
+const login = (request, response) => {
   const req = request;
   const res = response;
-  
+
   const username = `${req.body.username}`;
   const password = `${req.body.pass}`;
-  
-  if(!username || !password){
-    return res.status(400).json({error: 'RAWR! All All fields are required!'});
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'RAWR! All All fields are required!' });
   }
-  
-  return Account.AccountModel.authenticate(username, password, (err, account)=>{
-    if(err || !account){
-      return res.status(400).json({error: "Wrong username or password"});
+
+  return Account.AccountModel.authenticate(username, password, (err, account) => {
+    if (err || !account) {
+      return res.status(400).json({ error: 'Wrong username or password' });
     }
-    return res.json({redirect: '/maker'});
+
+    req.session.account = Account.AccountModel.toAPI(account);
+
+    return res.json({ redirect: '/maker' });
   });
 };
 
-const signup = (request, response) =>{
+const signup = (request, response) => {
   const req = request;
   const res = response;
 
@@ -38,14 +42,14 @@ const signup = (request, response) =>{
   req.body.pass = `${req.body.pass}`;
   req.body.pass2 = `${req.body.pass2}`;
 
-  if(!req.body.username || !req.body.pass || !req.body.pass2){
-    return res.status(400).json({error: 'RAWR! All fields are required!'});
+  if (!req.body.username || !req.body.pass || !req.body.pass2) {
+    return res.status(400).json({ error: 'RAWR! All fields are required!' });
   }
-  if(req.body.pass !== req.body.pass2){
-    return res.status(400).json({error: 'RAWR! Passwords do not match!'});
+  if (req.body.pass !== req.body.pass2) {
+    return res.status(400).json({ error: 'RAWR! Passwords do not match!' });
   }
 
-  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) =>{
+  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
     const accountData = {
       username: req.body.username,
       salt,
@@ -55,16 +59,19 @@ const signup = (request, response) =>{
 
     const savePromise = newAccount.save();
 
-    savePromise.then(()=> res.json({ redirect:'/maker'}));
+    savePromise.then(() => {
+      req.session.account = Account.AccountModel.toAPI(newAccount);
+      return res.json({ redirect: '/maker' });
+    });
 
-    savePromise.catch((err)=>{
+    savePromise.catch((err) => {
       console.log(err);
 
-      if(err.code === 11000){
-        return res.status(400).json({error:'Username already in use'});
+      if (err.code === 11000) {
+        return res.status(400).json({ error: 'Username already in use' });
       }
 
-      return res.status(400).json({error: 'An error occured'});
+      return res.status(400).json({ error: 'An error occured' });
     });
   });
 };
